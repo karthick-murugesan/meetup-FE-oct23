5435b62cf2cdaf4c2f25669e38b5d4b0c7b7ed3d
karthick-murugesan <karthick.murugesan@saama.com>
2023-10-26 12:25:47 +0530

    tailwind_added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+// App.js 
+
+import './index.css'; 
+import Feedback from './components/Feedback'; 
+import Banner from './components/Banner'; 
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const data = new FormData(event.target);
+
+  const value = data.get('username');
+
+  console.log({ value });
 }
+function App() { 
+return ( 
+	<div className="App"> 
+	{/* <Banner/>  */}
+	<Feedback/> 
+	</div> 
+); 
+} 
 
-export default App;
+export default App; 
---
+++ src/components/Banner.js
@@ -0,0 +1,10 @@
+// Banner.js 
+
+export default function Banner() { 
+	return ( 
+		<div className="h-24 w-full border-2 flex items-center 
+						justify-center bg-emerald-500 text-white"> 
+		<p className="text-2xl"> Welcome to Feedback Page!!!</p> 
+		</div> 
+	) 
+} 
